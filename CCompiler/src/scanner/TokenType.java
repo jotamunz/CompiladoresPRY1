48914826ec100f23a169f4d50c89e08a53b8d0dd
JoalG <53712282+JoalG@users.noreply.github.com>
2021-04-14 17:27:17 -0600

    Update TokenType.java

--- CCompiler/src/scanner/TokenType.java
+++ CCompiler/src/scanner/TokenType.java
@@ -2,20 +2,14 @@
 package scanner;
 
 public enum TokenType {
-    
     KEYWORD,
-    
     OPERATOR_ARITHMETIC,
     OPERATOR_LOGICAL,
     OPERATOR_STRUCTURE,
-    
     IDENTIFIER,
-    
-    // LITERALS
     INTEGER,
     FLOAT,
     STRING,
     CHAR,
-
     ERROR
 }
