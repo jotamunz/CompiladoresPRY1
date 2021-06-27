40c112190756f0a410b9b6e8dd4b0765e98abd68
Jose Pablo Munoz <jpmunoz@estudiantec.cr>
2021-06-27 17:01:29 -0600

    cleanup

--- CCompiler/src/model/compiler/translator/NasmConverter.java
+++ CCompiler/src/model/compiler/translator/NasmConverter.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package model.compiler.translator;
 
 import model.compiler.translator.SemanticRegisters.RsDO;
 
-/**
- *
- * @author joaqu
- */
 public class NasmConverter {
     private String nasmCode;
     private boolean isInCodeSection;
